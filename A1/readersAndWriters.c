277a49834dbd1f80798634b286cb233c3eca6168
Callum Williams <callum.a.williams@gmail.com>
2018-01-29 11:09:27 -0500

    Reader

--- A1/readersAndWriters.c
+++ A1/readersAndWriters.c
@@ -1,50 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
+#include <pthread.h>
 #include "readersAndWriters.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
 	
-	int n, m;
+	READERS *readers;
+	WRITERS *writers;
+	int m, n;
 	int i;
-	READER *readers;
-	WRITER *writers;
+	int *arg;
+	pthread_t *rp, *wp;
+	READERS *rtmp;
+	WRITERS *wtmp;
+	int ret1, ret2;
+	char *msg1 = "test1";
+	char *msg2 = "test2";
 	
 	if (argc < 3) {
-		printf("Insufficient arguments - please run as './readersAndWriters <N value> <M value>\n");
-		return 1;
+		printf("Please run as './readersAndWriters <# of readers> <# of writers>\n");
+		exit(1);
 	}
-	
 	n = atoi(argv[1]);
 	m = atoi(argv[2]);
 	
-	readers = malloc(sizeof(READER)*n);
-	writers = malloc(sizeof(WRITER)*m);
+	readers = malloc(sizeof(READERS)*n);
+	writers = malloc(sizeof(WRITERS)*m);
+	rtmp = malloc(sizeof(READERS)*n);
+	wtmp = malloc(sizeof(WRITERS)*m);
 	
-	if (!readers || !writers) {
-		printf("ERROR - Insufficient memory for readers/writers.\n");
-		return 1;
+	if (!readers || !writers || !rtmp || !wtmp) {
+		printf("Insufficient memory\n");
+		exit(1);
+	}
+	FILE_STORAGE = setupFile(m);
+	rp = malloc(sizeof(pthread_t)*n);
+	wp = malloc(sizeof(pthread_t)*m);
+	arg = malloc(sizeof(int));
+	if (!rp || !wp || !arg) {
+		printf("Insufficient memory for threads\n");
+		exit(1);
 	}
 	
-	srand(time(NULL));
-	
-	for (i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		
-		readers[i].READER_ID = i;
-		readers[i].WAIT_DURATION = rand() % 100 + 1;
-		printf("Reader %d waits for %d seconds\n", readers[i].READER_ID, readers[i].WAIT_DURATION);
+		rtmp = &readers[i];
+		readers[i] = setupReader(i);
+		pthread_create(&rp[i], NULL, readFromFile, (void *)rtmp);
 		
 	}
 	
-	for (i = 0; i < m; i++) {
+	for (int i = 0; i < n; i++) {
 		
-		writers[i].WRITER_ID = i;
-		writers[i].WAIT_DURATION = rand() % 100 + 1;
-		printf("Writer %d waits for %d seconds\n", writers[i].WRITER_ID, writers[i].WAIT_DURATION);
-
+		pthread_join(rp[i], NULL);
 		
 	}
+	
+	//ret1 = pthread_create(&t1, NULL, printMsg, (void *)msg1);
+	//ret2 = pthread_create(&t2, NULL, printMsg, (void *)msg2);
+	
+	//printf("%d and %d\n", ret1, ret2);
+	
+	//pthread_join(t1, NULL);
+	//pthread_join(t2, NULL);
 	
 	return 0;
 	
--- A1/readersAndWriters.h
+++ A1/readersAndWriters.h
@@ -1,16 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
 
-typedef struct r {
-	
-	int READER_ID;
-	int WAIT_DURATION;
-	
-}READER;
+char *FILE_STORAGE;
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+int MAX_READ_COUNT = 10;
 
-typedef struct w {
+typedef struct rea {
 	
-	int WRITER_ID;
-	int WAIT_DURATION;
+	int id;
+	int read_count;
+	float wait_time;
 	
-}WRITER;
+}READERS;
 
+typedef struct wri {
+	
+	int id;
+	float wait_time;
+	
+}WRITERS;
+
+READERS setupReader(int id) {
+	
+	READERS r;
+	srand(time(NULL));
+	r.id = id;
+	r.read_count = 0;
+	r.wait_time = rand();
+	return r;
+	
+}
+
+WRITERS *setupWriter(int id) {
+	
+	WRITERS *w = malloc(sizeof(WRITERS));
+	srand(time(NULL));
+	w->id = id;
+	w->wait_time = rand();
+	return w;
+	
+}
+
+char *setupFile(int n) {
+	
+	int i;
+	char *f = malloc(sizeof(char)*n+1);
+	for (i = 0; i < n; i++) {
+		f = strcat(f, "0");
+	}
+	
+	return f;
+	
+}
+
+void *readFromFile(void *p) {
+	
+	int readc = 0;
+	READERS *r = (READERS *)p;
+	int id = r->id;
+	int wait = r->wait_time;
+	
+	while(readc < MAX_READ_COUNT) {
+		
+		//pthread_mutex_trylock(&mutex1);
+		printf("R%d has read:\t%s\n", id, FILE_STORAGE);
+		readc++;
+		//pthread_mutex_unlock(&mutex1);
+		
+	}
+	
+}
+/*
+void *writeToFile(void *p) {
+	
+	//get lock
+	//write to file
+	//release lock
+	//return string
+	
+}
+*/
